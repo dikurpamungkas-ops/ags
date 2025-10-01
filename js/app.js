caf7349eda3ac86ddc50015f85f08508f65ba6ff
dikurpamungkas-ops <dikur.pamungkas@gmail.com>
2025-10-01 18:19:53 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -28,16 +28,7 @@ function update(cursorPosition) {
 
 update(0);
 
-window.addEventListener("mousemove", (e) => {
-  if (timeline.isActive()) return;
 
-  xValue = e.clientX - window.innerWidth / 2;
-  yValue = e.clientY - window.innerHeight / 2;
-
-  rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
-
-  update(e.clientX);
-});
 
 if (window.innerWidth >= 725) {
   main.style.maxHeight = `${window.innerWidth * 0.6}px`;
